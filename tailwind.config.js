1d6468c169ae5d9febf23f029f50f1ec06017665
SylvinIsamaza <isamazasylvin@gmail.com>
2023-12-03 11:28:10 +0200

    Making it dynamic

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = withMT({
   darkMode:"class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screen: {
+      extends: {
+        "400px":'400px'
+      }
+   
+    },
     extend: {},
   },
   plugins: [],
