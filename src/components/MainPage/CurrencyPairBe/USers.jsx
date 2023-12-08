375e48143e9012c5270d9242242b0a6b26ac6165
jodosjodos <jeandedieu2030@gmail.com>
2023-12-08 12:25:48 +0200

    well rolled

--- src/components/MainPage/CurrencyPairBe/USers.jsx
+++ src/components/MainPage/CurrencyPairBe/USers.jsx
@@ -19,87 +19,96 @@ function USers() {
     profile: "/images/profile1.png",
   };
   function flattenArray(arr) {
-    return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
+    return arr != null
+      ? arr.reduce(
+          (acc, val) =>
+            Array.isArray(val)
+              ? acc.concat(flattenArray(val))
+              : acc.concat(val),
+          []
+        )
+      : [];
   }
 
   // Example usage:
- 
+
   const modifiedComments = flattenArray(comments);
- console.log(modifiedComments)
-  
+  console.log(modifiedComments);
+
   useEffect(() => {
-    store.dispatch(loadComment(loggedInUser))
-  }, [store.dispatch])
-  
- 
+    store.dispatch(loadComment(loggedInUser));
+  }, [store.dispatch]);
+
   // let loggedInUser=null
-  console.log(comments)
+  console.log(comments);
   const isDarkTheme = savedTheme === "dark";
 
-  const [loginPopup,setLoginPopup]=useState(false)
-  const [comment,setComment]=useState("")
+  const [loginPopup, setLoginPopup] = useState(false);
+  const [comment, setComment] = useState("");
   const handleComment = () => {
-    loggedInUser!=null?store.dispatch(makeComment({ ...loggedInUser, msg: comment })):setLoginPopup(true)
-  }
+    loggedInUser != null
+      ? store.dispatch(makeComment({ ...loggedInUser, msg: comment }))
+      : setLoginPopup(true);
+  };
 
-  
   //  use margin for solution for making user div to  be fit to image and time divv
   return (
     <div className="flex flex-col gap-5  lg:mx-32 md:mx-25 mx-3    rounded-2xl   p-5 bg-[#E1E1E1] dark:bg-[#202020] ">
       <div className="flex flex-col  max-h-[500px] min-h-fit overflow-y-scroll   gap-12">
-        {modifiedComments.length>0&&modifiedComments.map((user) => (
-          <div
-            key={user.id}
-            className="flex flex-row relative  items-center justify-between"
-          >
-            <div className="flex flex-row md:gap-10 gap-3 justify-between items-center">
-              <div
-                className="hover:cursor-pointer"
-                onMouseEnter={() =>
-                  setIsHovered({ id: user.id, hovered: true })
-                }
-                onMouseLeave={() => setIsHovered({ id: "", hovered: false })}
-              >
-                {isHovered.id == user.id && isHovered.hovered && (
-                  <div className="flex flex-col gap-8 z-[1000] bg-white border-4 top-[-400px] left-0  rounded-2xl    p-5 absolute  dark:bg-[#202020] dark:border-[#333333]">
-                    <div className="flex flex-row items-center gap-10">
-                      <div>
-                        <img
-                          src={user.comment.profile}
-                          alt=" user profile"
-                          className="w-[91px] h-[91px]"
-                        />
-                      </div>
-                      <div>
-                        <h1 className="text-[#898989] font-bold text-2xl">
-                          {user.name}
-                        </h1>
-                        <h1 className="text-[#00FF57] font-semibold">
-                          {user.votes} UpVotes | {user.comments} comments
-                        </h1>
-                      </div>
-                    </div>
-                    <div className="flex flex-col gap-3">
-                      <h1 className="text-[#898989] font-bold text-xl">
-                        Badges
-                      </h1>
-                      <div className="flex flex-row items-center gap-12">
-                        {userProfile.badges.map((badge) => (
-                          <div
-                            key={badge.id}
-                            className={` ${
-                              badge.id == 4 ? "bg-[#ffe335]" : "bg-[#00FF57]"
-                            } px-3 py-2 rounded-lg`}
-                          >
-                            <img src={badge.image} alt="badge img" />
-                          </div>
-                        ))}
-                      </div>
-                      <div>
+        {modifiedComments.length > 0 &&
+          modifiedComments.map((user) => (
+            <div
+              key={user.id}
+              className="flex flex-row relative  items-center justify-between"
+            >
+              <div className="flex flex-row md:gap-10 gap-3 justify-between items-center">
+                <div
+                  className="hover:cursor-pointer"
+                  onMouseEnter={() =>
+                    setIsHovered({ id: user.id, hovered: true })
+                  }
+                  onMouseLeave={() => setIsHovered({ id: "", hovered: false })}
+                >
+                  {isHovered.id == user.id && isHovered.hovered && (
+                    <div className="flex flex-col gap-8 z-[1000] bg-white border-4 top-[-400px] left-0  rounded-2xl    p-5 absolute  dark:bg-[#202020] dark:border-[#333333]">
+                      <div className="flex flex-row items-center gap-10">
                         <div>
-                          <h1 className="text-[#898989] font-bold text-xl">
-                            Top Comments
+                          <img
+                            src={user.comment.profile}
+                            alt=" user profile"
+                            className="w-[91px] h-[91px]"
+                          />
+                        </div>
+                        <div>
+                          <h1 className="text-[#898989] font-bold text-2xl">
+                            {user.name}
                           </h1>
+                          <h1 className="text-[#00FF57] font-semibold">
+                            {user.votes} UpVotes | {user.comments} comments
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="flex flex-col gap-3">
+                        <h1 className="text-[#898989] font-bold text-xl">
+                          Badges
+                        </h1>
+                        <div className="flex flex-row items-center gap-12">
+                          {userProfile.badges.map((badge) => (
+                            <div
+                              key={badge.id}
+                              className={` ${
+                                badge.id == 4 ? "bg-[#ffe335]" : "bg-[#00FF57]"
+                              } px-3 py-2 rounded-lg`}
+                            >
+                              <img src={badge.image} alt="badge img" />
+                            </div>
+                          ))}
+                        </div>
+                        <div>
+                          <div>
+                            <h1 className="text-[#898989] font-bold text-xl">
+                              Top Comments
+                            </h1>
 
                             <div className="flex flex-row justify-between items-center">
                               <div className="text-[#898989] font-semibold">
@@ -184,7 +193,6 @@ function USers() {
           name="message"
           id="message"
           placeholder="Type your message"
-          value={comment}
           className="bg-[#898989] text-white placeholder:text-white outline-none w-[85%] px-3 py-4 rounded-2xl font-semibold dark:placeholder:text-[#636363] dark:text-[#636363]"
           onChange={(e) => setComment(e.target.value)}
         />
